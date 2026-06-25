a82bc4fc63a7219c5f6df0cfd231a900b67b1494
gulbin-dev <gulbindev@gmail.com>
2026-06-26 07:21:26 +0800

    style: update correct img path for  background footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,12 @@ export default function Footer() {
       <div className="absolute inset-0">
         <div className="bg-font-dark/40 absolute inset-0 z-1"></div>
         <img
-          src="/footer-1440.png"
-          srcSet="/footer-480.png 480w, /footer-800.png 800w, /footer-1440.png 1440w, /footer-1440@2x.png 2880w"
+          src="/footer-1440.webp"
+          srcSet="/footer-480.webp 480w, /footer-800.webp 800w, /footer-1440.webp 1440w, /footer-1440@2x.webp 2880w"
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 100vw"
           loading="lazy"
           alt=""
-          className="absolute inset-0 -z-1 h-full w-full object-cover"
+          className="absolute inset-0 z-1 h-full w-full object-cover blur-[2px]"
         />
         <Attribution>
           <p>
@@ -40,11 +40,14 @@ export default function Footer() {
           height={80}
           className="aspect-square"
         />
-        <p>
+        <p className="text-size-sm pt-2">
           Travel Expore is a demo website made by Front-end developer Joshua
           Glenn R. Gulbin. This demonstrates his front-end developing skill on
           building an optimize GSAP animated landing page.
         </p>
+        <nav>
+          <ul className="flex gap-1.5"></ul>
+        </nav>
         <p>All rights reserved.</p>
       </div>
     </footer>
