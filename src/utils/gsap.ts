4b741672a6effe3aeee4247fd113a741466d0b17
gulbin-dev <gulbindev@gmail.com>
2026-06-26 04:21:07 +0800

    refractor: remove CSSPlugin
    feat: added reduce-motion media query

--- src/utils/gsap.ts
+++ src/utils/gsap.ts
@@ -2,11 +2,12 @@ import { gsap } from "gsap";
 import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger, useGSAP, gsap);
 
 const mediaQueries = {
   isMobileScreen: "(max-width: 480px)",
   isTabletScreen: "(min-width: 481px and max-width: 990px)",
   isDesktopScreen: "(min-width: 901px)",
+  isReduceMotion: "(prefers-reduced-motion: reduce)",
 };
 export { gsap, mediaQueries, ScrollSmoother, ScrollTrigger, useGSAP };
