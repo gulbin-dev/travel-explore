41d0c98db4f899bc00ec68b2803f820576f4f9b7
gulbin-dev <gulbindev@gmail.com>
2026-06-27 13:35:48 +0800

    refactor: add multiple images on different sources

--- src/utils/land-sites.ts
+++ src/utils/land-sites.ts
@@ -13,12 +13,54 @@ export const data = [
           },
           source:
             "https://unsplash.com/photos/group-of-fishing-boats-on-body-of-water-_EtORiyLq6s?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/el-nido/el-nido-1.webp`,
+        attribution: {
+          photographer: {
+            profile:
+              "https://unsplash.com/@cjtagupa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+            name: "Cris Tagupa",
+          },
+          source:
+            "https://unsplash.com/photos/aerial-photo-of-body-of-water-between-mountains-9ZXHUr5aCwM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/el-nido/el-nido-2.webp`,
+        attribution: {
+          photographer: {
+            profile:
+              "https://unsplash.com/@mariovr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+            name: "Mario von Rotz",
+          },
+          source:
+            "https://unsplash.com/photos/an-aerial-view-of-a-beach-surrounded-by-trees-F4jg3IbiIOE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/el-nido/el-nido-3.webp`,
+        attribution: {
+          photographer: {
+            profile:
+              "https://unsplash.com/@jmtorres16?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+            name: "J Torres",
+          },
+          source:
+            "https://unsplash.com/photos/a-couple-of-boats-that-are-sitting-in-the-water-d5YCfxzetJs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
         },
       },
     ],
     name: "El Nido, Palawan",
     details: {
       location: "El Nido, Palawan, MIMAROPA, Philippines",
+      mapLocation:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.805133667211!2d119.41738148784974!3d11.202049009147618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33b7aaaa56cc4f0f%3A0x6702c107e035f70!2sEl%20Nido%20Airport!5e0!3m2!1sen!2sph!4v1782467553421!5m2!1sen!2sph",
       description:
         "This world-renowned paradise is globally acclaimed for its dramatic, towering limestone cliffs, hidden secret beaches, and pristine crystal-clear lagoons. Tucked away in northern Palawan, it serves as a gateway to the spectacular Bacuit Archipelago and features rich biodiversity both on land and underwater. Travelers flock here to experience premium island-hopping tours, dive into thriving marine sanctuaries, and view stunning golden hour sunsets from Marimegmeg Beach. It consistently ranks among the top tropical destinations in the world, captivating eco-tourists and luxury travelers alike.",
       activities:
@@ -39,12 +81,52 @@ export const data = [
           },
           source:
             "https://unsplash.com/photos/people-on-beach-during-daytime-Rdxo1VM8V5g?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/malcapuya/malcapuya-island-1.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@symeon-ekizoglou-1107605/",
+            name: "Symeon Ekizoglou",
+          },
+          source:
+            "https://www.pexels.com/photo/aerial-shot-of-an-island-3948079/",
+          provider: "Pexels",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/malcapuya/malcapuya-island-2.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@arjbga/",
+            name: "John Bryan Gray Agbagala",
+          },
+          source:
+            "https://www.pexels.com/photo/aerial-shot-of-landscape-8096639/",
+          provider: "Pexels",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/malcapuya/malcapuya-island-3.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@julian-ez-3298208/",
+            name: "Julián Ez",
+          },
+          source:
+            "https://www.pexels.com/photo/an-aerial-photography-of-a-beach-with-boats-on-the-seashore-5621737/",
+          provider: "Pexels",
         },
       },
     ],
     name: "Malcapuya Island, Palawan",
     details: {
       location: "Malcapuya Island, Coron, Palawan, MIMAROPA, Philippines",
+      mapLocation:
+        "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3905.6426351315304!2d120.11473898520627!3d11.790193137059749!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33b9ff16f649d8ef%3A0x3214f487f2894ba0!2sMalcapuya%20Island%20Beach%20Resort!5e0!3m2!1sen!2sph!4v1782467990065!5m2!1sen!2sph",
+
       description:
         "This stunning, secluded tropical escape is celebrated for its breathtaking stretch of powdery, ultra-white sand and exceptionally clear turquoise waters. Situated a couple of hours away from Coron town, it offers a raw, untouched island experience far from the typical tourist crowds. It attracts visitors seeking a peaceful day of sunbathing under rows of coconut palms or swimming in calm, shallow waters. The vibrant coral gardens thriving just meters from the shore make it a top destination for underwater exploration.",
       activities:
@@ -65,12 +147,51 @@ export const data = [
           },
           source:
             "https://unsplash.com/photos/a-colorful-surfboard-sitting-on-a-beach-next-to-a-life-guard-tower-u0wbwPCGlQo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/boracay/boracay-island-1.webp`,
+
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@dada/",
+            name: "Darwin Frivaldo",
+          },
+          source:
+            "https://www.pexels.com/photo/coconut-tree-near-body-of-water-11686011/",
+          provider: "Pexels",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/boracay/boracay-island-2.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@dada/",
+            name: "Darwin Frivaldo",
+          },
+          source: "https://www.pexels.com/photo/boat-on-shore-11686045/",
+          provider: "Pexels",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/boracay/boracay-island-3.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@dada/",
+            name: "Mark  Escalante",
+          },
+          source:
+            "https://www.pexels.com/photo/tropical-beach-with-palm-trees-9663652/",
+          provider: "Pexels",
         },
       },
     ],
     name: "Boracay Island, Aklan",
     details: {
       location: "Boracay Island, Malay, Aklan, Western Visayas, Philippines",
+      mapLocation:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13128.818968130185!2d121.92660247864742!3d11.959179160068746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a53d8abc94056d%3A0xfef131a6a4f0dbf9!2sBoracay%20Island%20Philippines!5e0!3m2!1sen!2sph!4v1782468088431!5m2!1sen!2sph",
       description:
         "This world-famous destination is iconic for its legendary four-kilometer White Beach, powder-soft sand that stays cool even in hot weather, and spectacular sunsets. Following its successful environmental rehabilitation, the island beautifully balances its thriving commercial center with restored natural marine life. It draws massive international crowds for its exciting day activities and its legendary, vibrant beachside nightlife. The island is also a major hub for kitesurfing and windsurfing at Bulabog Beach during the windy season.",
       activities:
@@ -91,12 +212,51 @@ export const data = [
           },
           source:
             "https://unsplash.com/photos/green-trees-near-body-of-water-under-blue-sky-during-daytime-7DokcE_mwtE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/coron/coron-1.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@popoy-dev-248507616/",
+            name: "Popoy Dev",
+          },
+          source:
+            "https://www.pexels.com/photo/beautiful-lake-and-island-13518466/",
+          provider: "Pexels",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/coron/coron-2.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@imagevain/",
+            name: "Leon Macapagal",
+          },
+          source:
+            "https://www.pexels.com/photo/top-view-photo-of-ocean-during-daytime-2346007/",
+          provider: "Pexels",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/coron/coron-3.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@mark-c-728748121/",
+            name: "Mark C",
+          },
+          source:
+            "https://www.pexels.com/photo/boats-anchored-by-the-islands-of-the-palawan-archipelago-18372500/",
+          provider: "Pexels",
         },
       },
     ],
     name: "Coron, Palawan",
     details: {
       location: "Coron, Palawan, MIMAROPA, Philippines",
+      mapLocation:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124915.96615484537!2d120.06097534739995!3d11.931238172740969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33ba1d0cd171fadb%3A0x5d982357f5ef1b21!2sTwin%20Lagoon!5e0!3m2!1sen!2sph!4v1782468186555!5m2!1sen!2sph",
       description:
         "This spectacular eco-tourism hub is globally recognized for its remarkably preserved World War II Japanese shipwrecks and towering karst landscapes. Surrounded by dramatic cliffs, it hides mesmerizing emerald-green lakes and lagoons that contain a mix of salt and fresh water. It attracts scuba divers from all over the world who want to explore underwater history and vibrant coral reefs. The town also serves as a peaceful jumping-off point for exploring the wild safari animals of Calauit Island nearby.",
       activities:
@@ -117,6 +277,7 @@ export const data = [
           },
           source:
             "https://unsplash.com/photos/a-beach-with-a-mountain-in-the-background-7tn2pxCt3x0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
         },
       },
       {
@@ -129,6 +290,7 @@ export const data = [
           },
           source:
             "https://unsplash.com/photos/a-beach-with-clear-blue-water-and-mountains-in-the-background-NawUdcgiBzI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
         },
       },
       {
@@ -141,12 +303,27 @@ export const data = [
           },
           source:
             "https://unsplash.com/photos/man-sits-overlooking-a-large-cross-in-the-ocean-AnkKSHqBha8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+          provider: "Unsplash",
+        },
+      },
+      {
+        image: `${imagekitUrlEndpoint}/card-img/camiguin/camiguin-3.webp`,
+        attribution: {
+          photographer: {
+            profile: "https://www.pexels.com/@jayper-kim-pillerin-385879/",
+            name: "Jayper Kim Pillerin",
+          },
+          source:
+            "https://www.pexels.com/photo/sunken-cemetery-cross-in-camiguin-philippines-36105571/",
+          provider: "Pexels",
         },
       },
     ],
     name: "Camiguin Island",
     details: {
       location: "Camiguin Province, Northern Mindanao, Philippines",
+      mapLocation:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63022.45014570809!2d124.6776309225258!3d9.163210615965188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3300708dce1f37a5%3A0x6405585d5e292f9a!2sSunken%20Cemetery!5e0!3m2!1sen!2sph!4v1782468261832!5m2!1sen!2sph",
       description:
         "Known as the 'Island Born of Fire', this unique pear-shaped province boasts seven volcanoes and a dense concentration of natural wonders. It is highly famous for its rich volcanic topography, which has created a landscape filled with therapeutic hot and cold springs. The island attracts nature lovers who prefer a quiet, eco-centric escape away from mainstream commercialization. Its most iconic sight is a stark white sandbar that completely disappears and reappears depending on the changing ocean tides.",
       activities:
