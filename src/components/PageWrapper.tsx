78404c7594652460ae60c591efb9b8c94c27fb54
gulbin-dev <gulbindev@gmail.com>
2026-06-27 13:39:34 +0800

    refactor: render ModalMap and ImageViewerModal

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,8 +2,9 @@ import { useGSAP, ScrollSmoother } from "@utils/gsap";
 import { useRef } from "react";
 import { store } from "@/utils/redux-toolkit/store";
 import { Provider } from "react-redux";
-import ModalWrapper from "@/routes/-component/ModalWrapper";
+import ImageViewerModalWrapper from "@/routes/-component/ImageViewerModalWrapper";
 import { ClientOnly } from "@tanstack/react-router";
+import ModalMapWrapper from "@/routes/-component/ModalMapWrapper";
 
 export default function PageWrapper({
   children,
@@ -26,7 +27,8 @@ export default function PageWrapper({
   return (
     <Provider store={store}>
       <ClientOnly>
-        <ModalWrapper />
+        <ImageViewerModalWrapper />
+        <ModalMapWrapper />
       </ClientOnly>
       <div ref={containerRef} id="smooth-wrapper">
         <div id="smooth-content">
