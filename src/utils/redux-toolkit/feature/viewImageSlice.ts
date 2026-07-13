376ab2e1ff279896b0ea470e7a95152507bd6307
gulbin-dev <gulbindev@gmail.com>
2026-07-13 09:10:57 +0800

    refactor: add full screen view slice for better state management

--- src/utils/redux-toolkit/feature/viewImageSlice.ts
+++ src/utils/redux-toolkit/feature/viewImageSlice.ts
@@ -1,32 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import type { ItemProp } from "@utils/types";
+import type { ActiveItemProp } from "@utils/types";
 
 interface ToggleState {
   isToggled: boolean;
-  activeItem: ItemProp | null;
+  isFullScreen: boolean;
+  activeItem: ActiveItemProp;
+  fullScreenImageSource: string;
 }
 
 const initialState: ToggleState = {
   isToggled: false,
+  isFullScreen: false,
   activeItem: null,
+  fullScreenImageSource: "",
 };
 
 export const isImageOnViewSlice = createSlice({
   name: "isImageOnView",
   initialState,
   reducers: {
-    setImageOnView: (state, action: PayloadAction<ItemProp | null>) => {
+    setImageOnView: (state, action: PayloadAction<ActiveItemProp | null>) => {
       if (action.payload === null) {
         state.isToggled = false;
         state.activeItem = null;
-      } else {
-        state.isToggled = !state.isToggled;
-        state.activeItem = state.isToggled ? action.payload : null;
+        return;
       }
+      state.isToggled = true;
+      state.activeItem = action.payload;
+    },
+    setFullScreenView: (state, action: PayloadAction<string>) => {
+      if (action.payload === "") {
+        state.isFullScreen = false;
+        state.fullScreenImageSource = "";
+        return;
+      }
+      state.isFullScreen = true;
+      state.fullScreenImageSource = action.payload;
     },
   },
 });
 
-export const { setImageOnView } = isImageOnViewSlice.actions;
+export const { setImageOnView, setFullScreenView } = isImageOnViewSlice.actions;
 export default isImageOnViewSlice.reducer;
