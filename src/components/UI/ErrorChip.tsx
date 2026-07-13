26bb00e1fa5a5a4adeb6bc09033e2e7dd2689e82
gulbin-dev <gulbindev@gmail.com>
2026-07-13 09:12:45 +0800

    feat: add error and loading image UI chip

---
+++ src/components/UI/ErrorChip.tsx
@@ -0,0 +1,7 @@
+export default function ErrorChip() {
+  return (
+    <div className="bg-primary/10 text-primary w-fit rounded-2xl p-1 backdrop-blur-sm">
+      <p className="flex max-w-fit gap-1">An error occured...</p>
+    </div>
+  );
+}
