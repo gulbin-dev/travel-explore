b3f8039e238ee35f627bfbabdd906e86be8968da
gulbin-dev <gulbindev@gmail.com>
2026-07-13 09:14:52 +0800

    chore: add new icons

--- src/utils/icons.tsx
+++ src/utils/icons.tsx
@@ -1,6 +1,12 @@
 import Facebook from "/social-icons/Facebook.svg";
 import Instagram from "/social-icons/Instagram.svg";
-import { Link, MapPinIcon } from "lucide-react";
+import {
+  Link,
+  MapPinIcon,
+  LoaderCircle,
+  LoaderCircleIcon,
+  ImageOffIcon,
+} from "lucide-react";
 
 export const FacebookIcon = function FacebookIcon() {
   return <img src={Facebook} alt="Share tourist site on Facebook" />;
@@ -11,6 +17,11 @@ export const InstagramIcon = function InstagramIcon() {
 };
 
 export const LinkIcon = <Link size={48} />;
+export const LoaderIcon = <LoaderCircle size={24} />;
+export const ErrorThumbnailIcon = <ImageOffIcon size={90} />;
+export const LoaderThumbnailIcon = (
+  <LoaderCircleIcon size={90} width={3} className="text-primary" />
+);
 export const LocataionPinIcon = (
   <MapPinIcon size={24} color="var(--color-cta)" />
 );
