48704fecb155bd09da2e5d65b573f0310876e95e
gulbin-dev <gulbindev@gmail.com>
2026-07-13 09:12:16 +0800

    style: apply custom style via component prop

--- src/components/UI/Attribution.tsx
+++ src/components/UI/Attribution.tsx
@@ -1,10 +1,14 @@
 export default function Attribution({
+  className,
   children,
 }: {
+  className?: string;
   children: React.ReactNode;
 }) {
   return (
-    <div className="text-primary absolute bottom-1 left-1 z-10 flex">
+    <div
+      className={`text-primary absolute bottom-1 left-1 z-10 flex ${className}`}
+    >
       <span className="bg-primary block h-7 w-1"></span>{" "}
       <div className="bg-primary/10 rounded-xs p-1.5 backdrop-blur-2xl">
         {children}
